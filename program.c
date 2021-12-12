c22d2cc442b093705ffbd5d4c741bd4c3d995a90
Pini <marc6571us@gmail.com>
2021-12-13 00:09:59 +0100

    Updated program.c

--- program.c
+++ program.c
@@ -45,7 +45,8 @@ void calculate_goal_difference();
 //unsigned int hash();
 //int line_numbers_in_file();
 
-int main(){
+int main()
+{
     Match match[AMOUNT_OF_MATCHES];
     Team team[AMOUNT_OF_TEAMS];
 
@@ -72,16 +73,19 @@ int main(){
     printf("Lines: %d", count + 1);
     return count + 1;
 }*/
-void read_matches_from_file(Match match[]){
+void read_matches_from_file(Match match[])
+{
     FILE *fp = fopen(PATH_TO_FILE, "r");
 
-    if(fp){
+    if (fp)
+    {
         Match new_match;
         int number = 0;
-        
-        while (fscanf(fp, "%s     %s %s     %s - %s     %d - %d     %d", 
-            new_match.day, new_match.date, new_match.time, new_match.team_1, new_match.team_2, 
-            &(new_match.goal_1), &(new_match.goal_2), &(new_match.spectator_count)) == 8){
+
+        while (fscanf(fp, "%s     %s %s     %s - %s     %d - %d     %d",
+                      new_match.day, new_match.date, new_match.time, new_match.team_1, new_match.team_2,
+                      &(new_match.goal_1), &(new_match.goal_2), &(new_match.spectator_count)) == 8)
+        {
 
             match[number] = new_match;
             number++;
@@ -90,15 +94,18 @@ void read_matches_from_file(Match match[]){
     }
 }
 
-void display_matches(Match *match){
-    for(int i = 0; i < AMOUNT_OF_MATCHES; i++){
+void display_matches(Match *match)
+{
+    for (int i = 0; i < AMOUNT_OF_MATCHES; i++)
+    {
         printf("\n%s     %s %s     %-3s - %-3s     %2d - %-2d     %d",
-        match[i].day, match[i].date, match[i].time, match[i].team_1, match[i].team_2,
-        match[i].goal_1, match[i].goal_2, match[i].spectator_count);
+               match[i].day, match[i].date, match[i].time, match[i].team_1, match[i].team_2,
+               match[i].goal_1, match[i].goal_2, match[i].spectator_count);
     }
 }
 
-void calculate_team_stats(Match *match, Team *team){
+void calculate_team_stats(Match *match, Team *team)
+{
     set_name(team);
     calculate_team_points(match, team);
     calculate_team_goals_scored(match, team);
@@ -106,7 +113,8 @@ void calculate_team_stats(Match *match, Team *team){
     calculate_goal_difference(team);
 }
 
-void set_name(Team *team){
+void set_name(Team *team)
+{
     char team_name[][4] = {"SDR", "FCM", "ACH", "RFC", "LBK", "AaB", "BIF", "FCN", "OB", "FCK", "AGF", "VB"};
     int length = sizeof(team_name) / sizeof(int);
 
@@ -116,61 +124,73 @@ void set_name(Team *team){
     }
 }
 
-void calculate_team_points(Match *match, Team *team){
+void calculate_team_points(Match *match, Team *team)
+{
     for (int i = 0; i < AMOUNT_OF_TEAMS; i++)
     {
         team[i].points = 0;
         for (int j = 0; j < AMOUNT_OF_MATCHES; j++)
         {
-            if(!strcmp(team[i].name, match[j].team_1) && match[j].goal_1 > match[j].goal_2){
+            if (!strcmp(team[i].name, match[j].team_1) && match[j].goal_1 > match[j].goal_2)
+            {
                 team[i].points += 3;
             }
-            if(!strcmp(team[i].name, match[j].team_2) && match[j].goal_2 > match[j].goal_1){
+            if (!strcmp(team[i].name, match[j].team_2) && match[j].goal_2 > match[j].goal_1)
+            {
                 team[i].points += 3;
             }
-            if(!strcmp(team[i].name, match[j].team_1) && match[j].goal_1 == match[j].goal_2){
+            if (!strcmp(team[i].name, match[j].team_1) && match[j].goal_1 == match[j].goal_2)
+            {
                 team[i].points += 1;
             }
-            if(!strcmp(team[i].name, match[j].team_2) && match[j].goal_2 == match[j].goal_1){
+            if (!strcmp(team[i].name, match[j].team_2) && match[j].goal_2 == match[j].goal_1)
+            {
                 team[i].points += 1;
             }
         }
     }
 }
 
-void calculate_team_goals_scored(Match *match, Team *team){
+void calculate_team_goals_scored(Match *match, Team *team)
+{
     for (int i = 0; i < AMOUNT_OF_TEAMS; i++)
     {
         team[i].goals_scored = 0;
         for (int j = 0; j < AMOUNT_OF_MATCHES; j++)
         {
-            if(!strcmp(team[i].name, match[j].team_1)){
+            if (!strcmp(team[i].name, match[j].team_1))
+            {
                 team[i].goals_scored += match[j].goal_1;
-            } 
-            else if (!strcmp(team[i].name, match[j].team_2)){
+            }
+            else if (!strcmp(team[i].name, match[j].team_2))
+            {
                 team[i].goals_scored += match[j].goal_2;
             }
-        }   
+        }
     }
 }
 
-void calculate_team_goals_conceded(Match *match, Team *team){
+void calculate_team_goals_conceded(Match *match, Team *team)
+{
     for (int i = 0; i < AMOUNT_OF_TEAMS; i++)
     {
         team[i].goals_conceded = 0;
         for (int j = 0; j < AMOUNT_OF_MATCHES; j++)
         {
-            if(!strcmp(team[i].name, match[j].team_1)){
+            if (!strcmp(team[i].name, match[j].team_1))
+            {
                 team[i].goals_conceded += match[j].goal_2;
-            } 
-            else if (!strcmp(team[i].name, match[j].team_2)){
+            }
+            else if (!strcmp(team[i].name, match[j].team_2))
+            {
                 team[i].goals_conceded += match[j].goal_1;
             }
-        }   
+        }
     }
 }
 
-void calculate_goal_difference(Team *team){
+void calculate_goal_difference(Team *team)
+{
     for (int i = 0; i < AMOUNT_OF_TEAMS; i++)
     {
         team[i].difference = 0;
@@ -178,9 +198,10 @@ void calculate_goal_difference(Team *team){
     }
 }
 
-void display_team_stats(Team *team){
+void display_team_stats(Team *team)
+{
     printf("\nTEAM | POINTS | GOALS SCORED | GOALS CONCEDED | DIFFERENCE\n");
-    
+
     for (int i = 0; i < AMOUNT_OF_TEAMS; i++)
     {
         //printf("\n%s: %d", team[i].name, hash(team[i].name));
